fee550588022c032b858d601a6ad84bd0eb67d34
lenjor <1124614430@qq.com>
2017-12-31 10:54:58 +0800

    做好主页头尾，具体的使用方法见home.html，在文件头和文件尾include 相应的文件

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,4 +10,16 @@ class Index extends \think\Controller
 		initDB_M();//调用dbConfig.php的函数初始化数据库
         return $this->fetch();
 	}
+    public function home(){
+        //从控制器跳转到视图
+        //$list = db('article')->select();
+        //$article = db('article');
+        //$list = $article->query("select * from article limit 12");
+        // 把一个变量赋值给模版
+        // 第一个参数：模版变量
+        // 第二个参数：值
+        //$this->assign('article_list',$list);
+        //$this->assign('article_list',$list);
+        return $this->fetch();
+    }
 }
