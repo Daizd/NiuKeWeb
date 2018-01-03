606369fc127b513522e967a3376091f90c777a8f
SC-201705281309\Administrator <daizidong1@gmail.com>
2018-01-03 18:19:34 +0800

    修改帖子的表字段，添加分页，分类

--- application/forum/controller/Forum.php
+++ application/forum/controller/Forum.php
@@ -1,17 +1,26 @@
 <?php
 namespace app\forum\controller;
+import('ORG.Util.Page');// 导入分页类
 
 class Forum extends \think\Controller
 {
     public function forum()
-    {
-		$list = db('post')->select();
+    {	
+		$list = db('post')
+				->alias('p')
+				->field('p.post_id, p.post_title, c.cat_name')
+				->order('p.post_id desc')
+				->join('post_category c','p.post_cat_id = c.cat_id')
+				->paginate(10);
 		$this->assign('post_list',$list);
 		return $this->fetch();
 	}
 	
 	public function publish()
 	{
+		$post_cat_lists = db('post_category')->select();
+
+		$this->assign('post_cat_lists',$post_cat_lists);
 		return $this->fetch();
 	}
 	
@@ -19,9 +28,10 @@ class Forum extends \think\Controller
 		$add_data = input();
 
 		$add_data['create_time'] = time();
+		
 		db('post')->insert($add_data);
 	
-		$this->success('发表成功','index');
+		$this->success('发表成功','forum');
 	}
 	
 	public function preview(){
--- application/index/controller/dbConfig.php
+++ application/index/controller/dbConfig.php
@@ -6,7 +6,8 @@ define("DB_USER_NAME_M", "root");//数据库用户名
 define("DB_USER_PWD_M", "");//数据库用户密码
 define("DB_NAME_M", "niu_ke_db");//数据库
 define("TABLE_USER_M", "niu_ke_user");//用户表，表要带前缀niu_ke_
-define("TABLE_POST_M", "niu_ke_post");
+define("TABLE_POST_M", "niu_ke_post");//帖子表，
+define("TABLE_POST_CATEGORY_M", "niu_ke_post_category");//帖子类别表，
 
 function initDB_M(){
 	$con = mysqli_connect(DB_HOST_M, DB_USER_NAME_M, DB_USER_PWD_M); 
@@ -50,14 +51,38 @@ function initDB_M(){
 			avatar varchar(50)
 		)";
 		if (mysqli_query($con, $sql)){
-			echo "数据表 ". TABLE_USER_M ." 创建成功";
+			echo "数据表 ". TABLE_USER_M ." 创建成功<br/>";
 		} else {
-			echo "创建数据表错误: " . mysqli_error($con); 
+			echo "创建数据表". TABLE_USER_M ."错误: " . mysqli_error($con ."<br/>"); 
 		}
-		mysqli_query($con, "INSERT INTO " .TABLE_USER_M. "(user_name, user_pwd) VALUE('test', '123')" );
+		$result = mysqli_query($con, "INSERT INTO " .TABLE_USER_M. "(user_name, user_pwd) VALUE('test', '123')" );
+		if (!$result){
+			die (TABLE_USER_M."表数据插入失败! ". mysqli_error($con)."<br/>");
+		}
 	}
-
-
+	
+	$result = mysqli_query($con, "SHOW TABLES LIKE '". TABLE_POST_CATEGORY_M."'");
+	if (mysqli_num_rows($result) == 0){
+		$sql = "CREATE TABLE " . TABLE_POST_CATEGORY_M .
+		"(
+			cat_id int PRIMARY KEY AUTO_INCREMENT,
+			cat_name varchar(20)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+		if (mysqli_query($con, $sql)){
+			echo "数据表 ". TABLE_POST_CATEGORY_M ." 创建成功<br/>";
+		} else {
+			echo "创建数据表". TABLE_POST_CATEGORY_M ."错误: " . mysqli_error($con)."<br/>"; 
+		}
+		$result = mysqli_query($con, "INSERT INTO " .TABLE_POST_CATEGORY_M. "(cat_name) VALUE('求助')" );
+		if (!$result){
+			die (TABLE_POST_CATEGORY_M."表数据插入失败!  ". mysqli_error($con)."<br/>");
+		}
+		$result = mysqli_query($con, "INSERT INTO " .TABLE_POST_CATEGORY_M. "(cat_name) VALUE('讨论')" );
+		$result = mysqli_query($con, "INSERT INTO " .TABLE_POST_CATEGORY_M. "(cat_name) VALUE('面经')" );
+		$result = mysqli_query($con, "INSERT INTO " .TABLE_POST_CATEGORY_M. "(cat_name) VALUE('求职')" );
+		$result = mysqli_query($con, "INSERT INTO " .TABLE_POST_CATEGORY_M. "(cat_name) VALUE('招聘')" );
+	}
+	
 	$result = mysqli_query($con, "SHOW TABLES LIKE '". TABLE_POST_M."'");
 	if (mysqli_num_rows($result) == 0){
 		$sql = "CREATE TABLE " . TABLE_POST_M .
@@ -65,16 +90,26 @@ function initDB_M(){
 			post_id int PRIMARY KEY AUTO_INCREMENT,
 			post_title varchar(50) NOT NULL,
 			post_content varchar(255),
-			post_column varchar(255),
-			create_time int(11)
+			post_heat int,
+			post_cat_id int,
+			create_time int(11),
+			foreign key(post_cat_id) references niu_ke_post_category(cat_id)
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 		if (mysqli_query($con, $sql)){
-			echo "数据表 ". TABLE_POST_M ." 创建成功";
+			echo "数据表 ". TABLE_POST_M ." 创建成功<br/>";
 		} else {
-			echo "创建数据表错误: " . mysqli_error($con); 
+			echo "创建数据表". TABLE_POST_M ."错误: " . mysqli_error($con)."<br/>"; 
 		}
-		mysqli_query($con, "INSERT INTO " .TABLE_POST_M. "(post_title, post_content, post_column, create_time) 
-		VALUE('广州大学的学生竟然..', '广州大学的学生竟然都不打游戏', '吹水', 123)" );
+		$result = mysqli_query($con, "INSERT INTO " .TABLE_POST_M. "(post_title, post_content, post_heat, post_cat_id, create_time) 
+		VALUE('广州大学的学生竟然..', '广州大学的学生竟然都不打游戏', 1, 1, 123)" );
+		if (!$result){
+			die (TABLE_POST_M."表数据插入失败! ". mysqli_error($con)."<br/>");
+		}
+		for ($i=0; $i<100; $i++) {
+			$n = ($i)%5+1;
+			mysqli_query($con, "INSERT INTO " .TABLE_POST_M. "(post_title, post_content, post_heat, post_cat_id, create_time) 
+				VALUE('test', 'test', 1, " .$n. ", " .time(). ")");
+		} 
 	}
 
 	mysqli_close($con);
